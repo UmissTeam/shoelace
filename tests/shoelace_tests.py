ee07488634861d6d14b945cf4b8870c8c940286d
Dylan Guedes <djmgguedes@gmail.com>
2017-05-12 08:21:13 -0300

    Fixes tests
    Signed-off-by: Dylan Guedes <djmgguedes@gmail.com>

--- tests/shoelace_tests.py
+++ tests/shoelace_tests.py
@@ -5,18 +5,12 @@ from shoelace.sensors import Sensor, TemperatureSensor
 class TestSensor(Sensor):
     @classmethod
     def limiar(cls):
-        return 10
-
-    @classmethod
-    def maxitems(cls):
-        return 5
-
-    def alert_callback(self):
-        self.critical_event = True
-        self.alert_triggered = False
+        return 1
 
     def push_callback(self, item):
-        pass
+        if (self.diff(item, self.last_sended) > TestSensor.limiar()):
+            self.last_sended = item
+
 
 class ShoelaceTests(TestCase):
     def setUp(self):
@@ -26,43 +20,22 @@ class ShoelaceTests(TestCase):
         pass
 
     def test_limiar_method(self):
-        self.assertEqual(TestSensor.limiar(), 10.)
+        self.assertEqual(TestSensor.limiar(), 1.)
 
     def test_diff(self):
         test_sens = TestSensor()
         self.assertEqual(test_sens.diff(1, 2), 50.0)
 
-    def test_list_execution_with_critical_event(self):
+    def test_list_last_sended(self):
         test_sens = TestSensor()
-        test_sens.critical_event = False
         test_sens.push(400.987)
-        test_sens.push(400.987)
-        test_sens.push(400.987)
-        test_sens.push(400.987)
-        test_sens.push(400.987)
-        test_sens.push(400.987)
-        test_sens.push(400.987)
-        test_sens.push(400.987)
+        self.assertEqual(test_sens.last_sended, 400.987)
+        test_sens.push(400.989)
+        self.assertEqual(test_sens.last_sended, 400.987)
         test_sens.push(402.187)
         test_sens.push(406.987)
         test_sens.push(410.987)
         test_sens.push(420.987)
-        self.assertEqual(test_sens.lowest_value, 400.987)
-        self.assertEqual(test_sens.highest_value, 420.987)
-        test_sens.push(1020.987)
-        self.assertEqual(test_sens.critical_event, True)
-
-    def test_list_execution_without_critical_event(self):
-        test_sens = TestSensor()
-        test_sens.critical_event = False
-        test_sens.push(400.987)
-        test_sens.push(401.987)
-        test_sens.push(402.987)
-        test_sens.push(403.987)
-        test_sens.push(404.987)
-        test_sens.push(405.987)
-        test_sens.push(406.987)
-        self.assertEqual(test_sens.critical_event, False)
 
 if __name__ == '__main__':
         unittest.main()
